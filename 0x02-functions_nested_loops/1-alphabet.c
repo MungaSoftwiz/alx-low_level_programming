808366c7655af0b73333fce7fe90aaff398c15dc
MungaSoftwiz <mwachilumobm@gmail.com>
2023-02-21 09:43:31 -0800

    Print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_alphabet - Entry point
+  *
+  * program prints alphabets in lowercase
+  *
+  * Return: void
+  */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+	_putchar(letter);
+	}
+	_putchar('\n');
+}
+
