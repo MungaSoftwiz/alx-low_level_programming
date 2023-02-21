d8a07c2df5f68c6b02d9bc65cca47ca8e91bec19
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2023-02-21 12:55:59 -0800

    Print time from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,7 @@ int hour, minute;
 
 	for (hour = 0; hour <= 24 ; hour++)
 	{
-	for (minute = 60; minute <= 60; minute++)
+	for (minute = 0; minute <= 60; minute++)
 	{
 		_putchar((hour / 10) + '0');
 		_putchar((hour % 10) + '0');
