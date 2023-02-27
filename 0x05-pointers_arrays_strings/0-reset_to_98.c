4e4abf0fc7ab596254b8a224f0062ef9b9977740
MungaSoftwiz <mwachilumobm@gmail.com>
2023-02-27 07:35:13 -0800

    Takes pointer to integer and updates value to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
   * reset_to_98 - entry point
   * Description: a function that takes a pointer to an int as parameter and
   * updates the value it points to to 98
-  * @n: variable that holds address of integer variable 
+  * @n: variable that holds address of integer variable
   * Return: void
   */
 
