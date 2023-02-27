227ea6d010e67f4d3f424f32f7859626844f22d4
MungaSoftwiz <mwachilumobm@gmail.com>
2023-02-27 07:31:16 -0800

    Take pointer to integer and update value it points to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - entry point
+  * Description: a function that takes a pointer to an int as parameter and
+  * updates the value it points to to 98
+  * @n: variable that holds address of integer variable 
+  * Return: void
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
