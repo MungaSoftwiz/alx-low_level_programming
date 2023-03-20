7704b3ff259874a2397dfb8dd13b53f40260db33
MungaSoftwiz <mwachilumobm@gmail.com>
2023-03-20 14:04:26 -0700

    Define struct type with members

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - defines a new type with elememts
+  * @name: first member
+  * @age: seconf member
+  * @owner: third member
+  */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} new_dog;
+
+#endif
