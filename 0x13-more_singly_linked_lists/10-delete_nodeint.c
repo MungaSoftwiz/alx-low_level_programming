b454d761860be93fc27e9382b230b8246a3837cc
MungaSoftwiz <mwachilumobm@gmail.com>
2023-04-05 22:18:13 +0300

    Delete node at specific index correction

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 	i = 0;
-	while (i <= index)
+	while (i < index)
 	{
 		previous = current;
 		current = current->next;
