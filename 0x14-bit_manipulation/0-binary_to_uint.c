8dd84e6eedeb5daa2fbb72c7da0c928dafd930bc
MungaSoftwiz <mwachilumobm@gmail.com>
2023-04-07 14:34:27 +0300

    Convert a binary string into an u_integer

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function converts binary into an unsigned integer.
+ * @b: the string of binary digits to be converted.
+ *
+ * Return: the converted number or 0 if some characters are not 0 or 1.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int value = 0;
+	int i;
+
+
+	if (b == NULL)
+		return (0);
+	i = 0;
+	while (b[i] != '\0')
+	{
+	if (b[i] != '0' && b[i] != '1')
+		return (0);
+	value <<= 1;
+	if (b[i] == '1')
+	{
+		value += 1;
+	}
+	i++;
+	}
+	return (value);
+}
